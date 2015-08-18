92626cb587f6f9a16e6dd13ea5e3213ca6195aea
Jason Paryani <github@jparyani.com>
2015-08-18 15:31:12 -0700

    Add config for Sandstorm

---
+++ data/config.php
@@ -0,0 +1,18 @@
+<?php
+
+###
+# @name                 Configuration
+# @author               Tobias Reich
+# @copyright    2014 Tobias Reich
+###
+
+if(!defined('LYCHEE')) exit('Error: Direct access is not allowed!');
+
+# Database configuration
+$dbHost = 'localhost'; # Host of the database
+$dbUser = 'root'; # Username of the database
+$dbPassword = ''; # Password of the database
+$dbName = 'lychee'; # Database name
+$dbTablePrefix = ''; # Table prefix
+
+?>
